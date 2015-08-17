eea172587699516198f467f7168b2066fd983f60
Aleš Jiránek <AlesJiranek@users.noreply.github.com>
2015-08-17 19:14:49 +0200

    Revert "Update to current version"

--- src/Icon.js
+++ src/Icon.js
@@ -10,13 +10,11 @@ var Icon = React.createClass({
 
   propTypes: {
     name: PropTypes.string.isRequired,
-    className: PropTypes.string,
     size: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x']),
     rotate: PropTypes.oneOf(['45', '90', '135', '180', '225', '270', '315']),
     flip: PropTypes.oneOf(['horizontal', 'vertical']),
     fixedWidth: PropTypes.bool,
     spin: PropTypes.bool,
-    pulse: PropTypes.bool,
     stack: React.PropTypes.oneOf(['1x', '2x']),
     inverse: React.PropTypes.bool
   },
@@ -25,7 +23,7 @@ var Icon = React.createClass({
     /*jshint eqnull:true */
     var {
       name, size, rotate, flip, spin, fixedWidth, stack, inverse,
-      pulse, className, ...props
+      className, ...props
     } = this.props;
     var classNames = `fa fa-${name}`;
     if (size) {
@@ -42,9 +40,6 @@ var Icon = React.createClass({
     }
     if (spin) {
       classNames += ' fa-spin';
-    }
-    if (pulse) {
-      classNames += ' fa-pulse';
     }
     
     if (stack) {
